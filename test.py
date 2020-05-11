2f0a5554c0bbb0821f2c915549fc03a1900dfd26
Sebastian Snoer <seb.snoer@gmail.com>
2020-05-11 20:09:15 +0200

    Updating pom.xml for release

--- test.py
+++ test.py
@@ -4,31 +4,25 @@ def countlines(start, lines=0, begin_start=None):
     for thing in os.listdir(start):
         thing = os.path.join(start, thing)
         if os.path.isfile(thing):
-            if thing.endswith('.java'):
+            if thing.endswith('.java') or thing.endswith('.xml'):
                 with open(thing, 'r') as f:
-                    newlines = f.readlines()
-                    newlines = len(newlines)
-                    lines += newlines
+                    for line in f.readlines():
+                        if "2.10.4" in line: print(thing)
 
                     if begin_start is not None:
                         reldir_of_thing = '.' + thing.replace(begin_start, '')
                     else:
                         reldir_of_thing = '.' + thing.replace(start, '')
 
-                    #file = open("./"+begin_start)
-                    #print(file.read())
-                    #file.close()
-
     for thing in os.listdir(start):
         thing = os.path.join(start, thing)
         if os.path.isdir(thing):
-            lines = countlines(thing, lines, begin_start=start)
+            countlines(thing, lines, begin_start=start)
 
-    return lines
 	
 mode = input("1. All programming languges\n2. Java only ")
 if(mode == "2"):
-	input(countlines('io/'))
+	input(countlines('./'))
 else:
 	input(countlines('.'))
 	#E:\Code\Project Icespire\Icespire\src
